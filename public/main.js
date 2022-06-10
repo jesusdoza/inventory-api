3951a420a01a7227fb16b7c4b2d7f39a7305a1b6
jesusdoza <jesus.doza@gmail.com>
2022-06-09 19:53:27 -0500

    add query selectors for quantity manipulation

--- public/main.js
+++ public/main.js
@@ -1,31 +1,73 @@
 
 
-const update = document.querySelector('#update-button')
-const del = document.querySelector('#delete-button')
+const list = document.querySelector('#inventory')
 
-update.addEventListener('click',  _=>{
-    console.log('sent fetch from client')
-   fetch('/quotes',{
-       method: 'put',
-       headers: {'Content-Type': 'application/json'},
-       body: JSON.stringify({
-           name: 'bob',
-           quote: 'better added because we did not find any jesus'
-       })
-   })
 
-   .then(response =>{
-       if(response.ok){
-           return response.json()
-       }
-   })
-   .then(response=>{
-       console.log(`RESPONSE WAS ***************************`)
-       console.log(response)
-   })
-   .then(res =>{
-       window.location.reload(true)
-   })
+//add
+list.addEventListener('click', (event)=>{
+    //what was clicked
+    const target = event.target
+    console.log(event.target)
+
+    //what item row was clicked
+    const item = event.target.closest('.partnumber')
+    // console.log(`item is`)
+    // console.log( item)
+
+    //quantity of stock
+    const quantity = item.querySelector("input[type=number]")
+
+    //partnumber to use later to search database
+    const partId = item.querySelector('.partId').innerText
+     console.log( partId)
+
+    if(target.id=='reduce-button'){
+        console.log(`its reduce button`)
+        console.log( `quantity ${quantity.value}`)
+
+        if(quantity.value<=0){
+            quantity.value=0
+        }else{
+            quantity.value= +quantity.value - 1
+            console.log( `quantity ${quantity.value}`)
+        }
+       
+    }
+    else if(target.id=='add-button'){
+        console.log(`its reduce button`)
+        console.log( `quantity ${quantity.value}`)
+        
+        quantity.value= +quantity.value + 1
+        console.log( `quantity ${quantity.value}`)
+
+    }
+
+   
+
+
+
+
+
+//    fetch('/quotes',{
+//        method: 'put',
+//        headers: {'Content-Type': 'application/json'},
+//        body: JSON.stringify({
+          
+//        })
+//    })
+
+//    .then(response =>{
+//        if(response.ok){
+//            return response.json()
+//        }
+//    })
+//    .then(response=>{
+//        console.log(`RESPONSE WAS ***************************`)
+//        console.log(response)
+//    })
+//    .then(res =>{
+//        window.location.reload(true)
+//    })
 
 
 })
@@ -33,33 +75,3 @@ update.addEventListener('click',  _=>{
 
 
 
-//delete fetch
-del.addEventListener('click',  _=>{
-    console.log('sent fetch for a delete from client')
-   fetch('/quotes',{
-       method: 'delete',
-       headers: {'Content-Type': 'application/json'},
-       body: JSON.stringify({
-           name: 'bob',
-        //    quote: 'better added because we did not find any jesus'
-       })
-   })
-
-   .then(response =>{
-       if(response.ok){
-           return response.json()
-       }
-   })
-   .then(response=>{
-       console.log(`RESPONSE WAS ***************************`)
-       console.log(response)
-   })
-   .then(res =>{
-       window.location.reload(true)
-   })
-   .catch(err =>{
-       console.log(`error client side delete  ${err}`)
-   })
-
-
-})
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // import modules
-// const secrets = process.env.cata; //enviroment var for credentials else import it 
-const secrets = require('./secrets').cata_key
+const secrets = process.env.cata; //enviroment var for credentials else import it 
+// const secrets = require('./secrets').cata_key
 const express = require('express')
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
@@ -12,113 +12,6 @@ const uri = `mongodb+srv://${secrets}@cluster0.losdw.mongodb.net/?retryWrites=tr
 
 //instance of express
 const app = express();
-
-
-// inventory = [
-//     {partnumber:'3618046',
-//         model:'Celect',
-//         instock:0,
-//     },
-//     {partnumber:'3619037',
-//         model:'Celect',
-//         instock:0,
-//     },
-//     {partnumber:'3614473',
-//         model:'Celect',
-//         instock:0,
-//     },
-//     {partnumber:'3408303',
-//         model:'Celect Plus',
-//         instock:0,
-//     },
-//     {partnumber:'3408300',
-//         model:'Celect Plus',
-//         instock:0,
-//     },
-//     {partnumber:'3096662',
-//         model:'Celect Plus',
-//         instock:0,
-//     },
-//     {partnumber:'3944105',
-//         model:'ISC',
-//         instock:0,
-//     },
-//     {partnumber:'3944125',
-//         model:'ISC',
-//         instock:0,
-//     },
-//     {partnumber:3944124,
-//         model:'ISB',
-//         instock:0,
-//     },
-//     {partnumber:3945868,
-//         model:'ISB',
-//         instock:0,
-//     },
-//     {partnumber:3990517,
-//         model:'ISB',
-//         instock:0,
-//     },
-//     {partnumber:3942858,
-//         model:'ISB',
-//         instock:0,
-//     },
-//     {partnumber:3942335,
-//         model:'ISB',
-//         instock:0,
-//     },
-//     {partnumber:3492860,
-//         model:'ISB',
-//         instock:0,
-//     },
-//     {partnumber:3682729,
-//         model:'ISX EGR',
-//         instock:0,
-//     },
-//     {partnumber:3683289,
-//         model:'ISX EGR',
-//         instock:0,
-//     },
-//     {partnumber:3684009,
-//         model:'ISX EGR',
-//         instock:0,
-//     },
-//     {partnumber:3684275,
-//         model:'ISX EGR',
-//         instock:0,
-//     },
-//     {partnumber:4921776,
-//         model:'ISX',
-//         instock:0,
-//     },
-//     {partnumber:3681404,
-//         model:'ISX/ISM',
-//         instock:0,
-//     },
-//     {partnumber:3681405,
-//         model:'ISX/ISM',
-//         instock:0,
-//     },
-//     {partnumber:3408501,
-//         model:'ISX/ISM',
-//         instock:0,
-//     },
-//     {partnumber:3680509,
-//         model:'ISX NON EGR',
-//         instock:0,
-//     },
-//     {partnumber:3408426,
-//         model:'ISX NON EGR',
-//         instock:0,
-//     },
-//     {partnumber:3103533,
-//         model:'ISX NON EGR',
-//         instock:0,
-//     }
-
-
-// ]
-
 
         app.use(cors());
         app.set('view engine', 'ejs'); // for template
@@ -134,10 +27,16 @@ const app = express();
         const collection =  await db.collection('inventory');
         
         
+        app.post('/inventory',(request,response)=>{
+
+            response.send('post at inventory')
+
+        })
+
+
 
         app.get('/', async (request, response)=>{
-            try{
-                
+            try{  
                 const allInventory = await collection.find().toArray()
                 console.log(allInventory)
                 
@@ -149,8 +48,50 @@ const app = express();
             catch(err){
                 throw new Error(`error at root inventory load ${err}`)
             }
-            
-        
+        })
+
+
+
+
+        //update inventory amount
+         app.put('/inventory', async (request, response)=>{
+            console.log(`received put request on server to update using`)
+            console.log(request.body)
+
+            //{ //EXAMPLE OBJECT STRUCTURE
+            //  partnumber:3103533,
+            //  model:'ISX NON EGR',
+            //  instock:0,
+            //}
+            try {
+                const result = await collection.findOneAndUpdate(
+                    //query
+                    {
+                        partnumber : request.body.partnumber
+                    },
+                    
+                    {// update
+                        $set : {
+                            instock: request.body.instock
+                        }
+                    }
+                   ,
+                    //options
+                    {   //if query does not find anything insert it as new partnumber
+                        upsert: false
+                    }
+                )
+
+
+
+               response.redirect('/');
+               
+
+            } 
+            catch (error) {
+                
+            }
+
         })
 
 
@@ -163,125 +104,6 @@ const app = express();
 
     //instance of connect function
     let collection = connect()
-
-
-
-    
-
-
-        // app.get('/', async (request, response)=>{
-        //     try{
-                
-        //         const allInventory = await collection.find().toArray()
-        //         console.log(allInventory)
-                
-        //         // render index.ejs template passing in variable inventory holding allInventory
-        //         // response.render('index.ejs',{inventory:allInventory})
-                
-        //         response.end()
-        //     }
-        //     catch(err){
-        //         throw new Error(`error at root inventory load ${err}`)
-        //     }
-            
-        
-        // })
-    
-
-// MongoClient.connect(uri,{useUnifiedTopology: true, useUnifiedTopology: true,})
-//     .then(client =>{
-
-
-//         // database working with
-//         const db = client.db('Cata');
-
-//         //collection working on inside the database
-//         const quotesCollection = db.collection('inventory');
-
-//         console.log('connected to database ');
-
-//         // MIDDLEWARE 
-//         // ===================================================
-//         app.use(cors());
-//         app.set('view engine', 'ejs'); // for template
-//         app.use(bodyParser.urlencoded({extended:true})); //get body data
-//         app.use(bodyParser.json());
-//         app.use(express.static('public')) //use templates from folder
-       
-//         // MIDDLEWARE 
-//         // ===================================================
-        
-
-//         app.get('/', async (request, response)=>{
-
-
-//             try{
-//                 const allInventory = await quotesCollection.find().toArray()
-//                  //render index.ejs template passing in variable inventory holding allInventory
-//                 response.render('index.ejs',{inventory:allInventory})
-//                 response.end()
-//             }
-//             catch(err){
-//                 console.log(`error at root inventory load ${err}`)
-//             }
-           
-        
-//         })
-
-//         //update inventory amount
-
-//          app.put('/inventory', async (request, response)=>{
-//             console.log(`received put request on server to update using`)
-//             console.log(request.body)
-
-//             //{ //EXAMPLE OBJECT STRUCTURE
-//             //  partnumber:3103533,
-//             //  model:'ISX NON EGR',
-//             //  instock:0,
-//             //}
-
-
-//             try {
-//                 const result = await quotesCollection.findOneAndUpdate(
-//                     //query
-//                     {
-//                         partnumber : request.body.partnumber
-        
-//                     },
-                    
-//                     {// update
-//                         $set : {
-                           
-//                             instock: request.body.instock
-//                         }
-//                     }
-//                    ,
-//                     //options
-//                     {   //if query does not find anything insert it as new partnumber
-//                         upsert: false
-//                     }
-//                 )
-
-
-
-//                response.redirect('/');
-
-//             } 
-//             catch (error) {
-                
-//             }
-            
-          
-
-
-//         })
-           
-//     })//Mongo client end =========================================
-
-
-
-
-
 
 
 app.listen(process.env.PORT || PORT, ()=>{
