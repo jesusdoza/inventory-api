e4a08f8ef0be35e708f461cc853fab8a6483833d
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2022-12-04 21:32:48 -0600

    ADD signup route to api

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,18 @@
-const Router = require('express').Router();
-const apiController = require('../controllers/apiController.js')
-const  {ensureAuth,ensureGuest } = require('../middleware/auth')
+const Router = require("express").Router();
+const apiController = require("../controllers/apiController.js");
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
-const apiAuthController = require('../controllers/apiAuthController')
-
-
-
+const apiAuthController = require("../controllers/apiAuthController");
 
 /// @route /api/*
-Router.get('/',ensureAuth,apiController.getInventory)
-Router.post('/',ensureAuth, apiController.addItem)
-Router.delete('/',ensureAuth, apiController.removePart)
-Router.put('/update',ensureAuth,apiController.updatePart)
+Router.get("/", ensureAuth, apiController.getInventory);
+Router.post("/", ensureAuth, apiController.addItem);
+Router.delete("/", ensureAuth, apiController.removePart);
+Router.put("/update", ensureAuth, apiController.updatePart);
 
-Router.post('/login', apiAuthController.postLogin)
-Router.get('/logout', apiAuthController.logout)
+Router.post("/login", apiAuthController.postLogin);
+Router.get("/logout", apiAuthController.logout);
 
+Router.post("/signup", apiAuthController.postSignup);
 
-module.exports = Router
+module.exports = Router;
